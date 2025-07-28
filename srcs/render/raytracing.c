8f0ca91eb60430179120989afdeea343696252e0
ROLLAND <nathan.rolland066@gmail.com>
2025-07-28 19:26:07 +0200

    modif raytracing

--- srcs/render/raytracing.c
+++ srcs/render/raytracing.c
@@ -6,7 +6,7 @@
 /*   By: star <star@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 15:25:10 by star              #+#    #+#             */
-/*   Updated: 2025/07/26 16:24:07 by star             ###   ########.fr       */
+/*   Updated: 2025/07/28 19:24:08 by star             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,13 +47,13 @@ t_fcolor	shade_raytracing_pixel(t_data *data, t_ray ray)
 	t_fcolor	a_light;
 	t_hit		hit;
 
+	hit = intersectscene(data, ray);
+	if (hit.t <= 0 || hit.type == 3)
+		return ((t_fcolor){0.0f, 0.0f, 0.0f});
 	l_intensity = (t_fcolor){0.0, 0.0, 0.0};
 	a_light = scale_mlx_color(mlxcolor_to_fcolor(data->scene.a_light.color),
 			data->scene.a_light.ratio);
 	l_intensity = add_color(l_intensity, a_light);
-	hit = intersectscene(data, ray);
-	if (hit.t <= 0)
-		return ((t_fcolor){0.0f, 0.0f, 0.0f});
 	l_intensity = ft_color_ray(hit, data, ray, l_intensity);
 	hit.color = scalar_color(l_intensity, hit.color);
 	return (hit.color);
